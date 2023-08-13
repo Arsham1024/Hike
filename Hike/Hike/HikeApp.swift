f72cfd620c1ffacb597e904a542661cbf2cd34af
Arsham1024 <Arsham512@gmail.com>
2023-08-12 18:17:52 -0700

    create card view

---
+++ Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Arsham Mehrani on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
